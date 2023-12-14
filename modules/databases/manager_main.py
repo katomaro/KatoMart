806d5469098e1423077a8eb950ea11fcdf169ad1
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2023-12-14 14:04:59 -0300

    fix[databases]: remocao de caminhos relativos ao interpretador ao executar

--- modules/databases/manager_main.py
+++ modules/databases/manager_main.py
@@ -2,18 +2,22 @@
 
 import sqlite3
 
+from os.path import abspath, dirname
+
+
 class ManagerMain:
     """Esta classe é responsável por gerenciar o banco de dados principal."""
     
     def __init__(self):
-        self._conn = sqlite3.connect('main.sqlite3')
+        self.dir = dirname(abspath(__file__))
+        self._conn = sqlite3.connect(self.dir + '/main.sqlite3')
         self._cursor = self._conn.cursor()
         if isinstance(self, ManagerMain):
             self.__update_schema()
     
     def __update_schema(self):
         """Atualiza o schema do banco de dados, pelo mais atual."""
-        with open('main.sql', 'r', encoding='utf-8') as sql_file:
+        with open(self.dir + '/main.sql', 'r', encoding='utf-8') as sql_file:
             sql_commands = sql_file.read()
             self._cursor.executescript(sql_commands)
         self._conn.commit()

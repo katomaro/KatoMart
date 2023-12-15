ef82c95e4079f573005cac330b98d90d7b4bff6a
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2023-12-15 18:18:42 -0300

    refactor[databases]: altera a utilizacao de cursores e corrige acessos para hierarquia

--- modules/databases/manager_main.py
+++ modules/databases/manager_main.py
@@ -13,27 +13,30 @@ class ManagerMain:
         self.__should_update_schema = False
         if not self.main_database.exists():
             self.__should_update_schema = True
-        self._conn = sqlite3.connect(self.main_database)
-        self._conn.row_factory = sqlite3.Row
-        self._cursor = self._conn.cursor()
+        self._main_conn = sqlite3.connect(self.main_database)
+        self._main_conn.row_factory = sqlite3.Row
 
         if self.__should_update_schema:
             self.__update_schema()
 
     def __update_schema(self):
         """Atualiza o schema do banco de dados, pelo mais atual."""
+        cursor = self._main_conn.cursor()
         with open(self.main_db_dir / 'main.sql', 'r', encoding='utf-8') as sql_file:
             sql_commands = sql_file.read()
-            self._cursor.executescript(sql_commands)
-        self._conn.commit()
+            cursor.executescript(sql_commands)
+        self._main_conn.commit()
+        cursor.close()
         print('[DATABASE] Schema prncipal do banco de dados atualizado com sucesso.')
-        self._update_supported_platforms()
+        self.__update_supported_platforms()
 
-    def _update_supported_platforms(self):
+    def __update_supported_platforms(self):
         """Atualiza as plataformas suportadas pelo programa."""
+        cursor = self._main_conn.cursor()
         with open(self.main_db_dir / 'supported_platforms.sql', 'r', encoding='utf-8') as sql_file:
             sql_commands = sql_file.read()
-            self._cursor.executescript(sql_commands)
-        self._conn.commit()
-        total = self._conn.execute('select count(name) from Platforms').fetchone()[0]
+            cursor.executescript(sql_commands)
+        self._main_conn.commit()
+        cursor.close()
+        total = self._main_conn.execute('select count(name) from Platforms').fetchone()[0]
         print(f'[DATABASE] {total} Plataformas suportadas atualizadas com sucesso.')

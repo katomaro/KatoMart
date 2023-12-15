af95156b62c1207d8005138ad30d9302467ec3c3
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2023-12-15 18:49:02 -0300

    docs[databases]: type hints basico

--- modules/databases/manager_main.py
+++ modules/databases/manager_main.py
@@ -3,6 +3,7 @@
 import sqlite3
 
 from pathlib import Path
+from typing import Tuple
 
 from modules.accounts.abstract import Account
 
@@ -21,7 +22,7 @@ class ManagerMain:
         if self.__should_update_schema:
             self.__update_schema()
 
-    def __update_schema(self):
+    def __update_schema(self) -> None:
         """Atualiza o schema do banco de dados, pelo mais atual."""
         cursor = self._main_conn.cursor()
         with open(self.main_db_dir / 'main.sql', 'r', encoding='utf-8') as sql_file:
@@ -32,7 +33,7 @@ class ManagerMain:
         print('[DATABASE] Schema prncipal do banco de dados atualizado com sucesso.')
         self.__update_supported_platforms()
 
-    def __update_supported_platforms(self):
+    def __update_supported_platforms(self) -> None:
         """Atualiza as plataformas suportadas pelo programa."""
         cursor = self._main_conn.cursor()
         with open(self.main_db_dir / 'supported_platforms.sql', 'r', encoding='utf-8') as sql_file:
@@ -43,7 +44,7 @@ class ManagerMain:
         total = self._main_conn.execute('SELECT count(name) SELECT Platforms').fetchone()[0]
         print(f'[DATABASE] {total} Plataformas suportadas atualizadas com sucesso.')
 
-    def get_supported_platforms(self):
+    def get_supported_platforms(self) -> Tuple[sqlite3.Row, ...]:
         """Retorna as plataformas suportadas pelo programa."""
         cursor = self._main_conn.cursor()
         cursor.execute('SELECT * from Platforms')
@@ -51,7 +52,7 @@ class ManagerMain:
         cursor.close()
         return platforms
 
-    def insert_new_account(self, new_account) -> Account:
+    def insert_new_account(self, new_account: Account=Account('Invalida', '123', 1)) -> Account:
         """Insere uma nova conta no banco de dados."""
         cursor = self._main_conn.cursor()
         cursor.execute('INSERT OR IGNORE INTO Accounts (username, password, platform_id) values (?, ?, ?)',

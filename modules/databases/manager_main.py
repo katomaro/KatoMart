73f186958d071d831f10eead17ddbafe9330a31d
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2023-12-15 13:57:16 -0300

    refactor[databases]: pathlib syntatic sugar

--- modules/databases/manager_main.py
+++ modules/databases/manager_main.py
@@ -8,15 +8,20 @@ from pathlib import Path
 class ManagerMain:
     """Esta classe é responsável por gerenciar o banco de dados principal."""
     def __init__(self):
-        self.dir = str(Path(__file__).parent)
-        self._conn = sqlite3.connect(self.dir + '/main.sqlite3')
+        self.main_db_dir = Path(__file__).parent
+        self.main_database = self.main_db_dir / 'main.sqlite3'
+        self.__should_update_schema = False
+        if not self.main_database.exists():
+            self.__should_update_schema = True
+        self._conn = sqlite3.connect(self.main_database)
         self._cursor = self._conn.cursor()
-        if isinstance(self, ManagerMain):
+
+        if self.__should_update_schema:
             self.__update_schema()
 
     def __update_schema(self):
         """Atualiza o schema do banco de dados, pelo mais atual."""
-        with open(self.dir + '/main.sql', 'r', encoding='utf-8') as sql_file:
+        with open(self.main_db_dir / 'main.sql', 'r', encoding='utf-8') as sql_file:
             sql_commands = sql_file.read()
             self._cursor.executescript(sql_commands)
         self._conn.commit()

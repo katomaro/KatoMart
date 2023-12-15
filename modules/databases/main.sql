d55cd935f13a3f2f692218be9f02e9371750b0de
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2023-12-15 12:08:01 -0300

    feat[databases]: adiciona tabela de cursos

--- modules/databases/main.sql
+++ modules/databases/main.sql
@@ -5,7 +5,6 @@ CREATE TABLE IF NOT EXISTS Platforms (
     landing_page TEXT NOT NULL,
     content_delivery_type TEXT NOT NULL,
     last_accessed_at INTEGER DEFAULT 0,
-    has_errors INTEGER DEFAULT 0,
     ip_banned INTEGER DEFAULT 0
 );
 
@@ -21,4 +20,16 @@ CREATE TABLE IF NOT EXISTS Accounts (
     authentication_token_expires_at INTEGER DEFAULT 0,
     platform_id INTEGER REFERENCES Platforms(id) ON DELETE CASCADE,
     UNIQUE(username, platform_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Courses (
+    id INTEGER,
+    name TEXT NOT NULL,
+    description TEXT,
+    course_url TEXT NOT NULL,
+    downloaded_at INTEGER DEFAULT 0,
+    last_verified_at INTEGER DEFAULT 0,
+    platform_id INTEGER REFERENCES Platforms(id) ON DELETE CASCADE,
+    UNIQUE(id, platform_id)
 );

0a5f184432cf54ac88fba0e7f3b533e210c191a1
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2023-12-15 18:54:37 -0300

    PLAYGROUND! GOSTO DE GATOS E MINECRAFT! DRAGON VILLAGE COLLECTION EH BOM (server E-Asia)!

--- katomart.py
+++ katomart.py
@@ -1,3 +1,13 @@
 """Ponto de inicialização para um futuro server Flask"""
 
-print("Goodbye World! Alprazolam 2mg é bom")
+# from modules.databases.manager_main import ManagerMain
+# from modules.accounts.abstract import Account
+
+if __name__ == '__main__':
+    # manager = ManagerMain()
+    # print(manager.get_supported_platforms())
+    # account = Account('teste', 'teste', 1)
+    # a = manager.insert_new_account(account)
+    # print(a)
+
+    print("Goodbye World! Alprazolam 2mg é bom. Lembre-se que você deve ser acompanhado por um médico.")

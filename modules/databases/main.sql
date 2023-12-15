f972d59f03929287a451c088bb0de35d18e577c1
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2023-12-15 12:14:40 -0300

    docs[databases]: descricao de cada atributo das tabelas basicas

--- modules/databases/main.sql
+++ modules/databases/main.sql
@@ -1,35 +1,35 @@
 CREATE TABLE IF NOT EXISTS Platforms (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    description TEXT,
-    landing_page TEXT NOT NULL,
-    content_delivery_type TEXT NOT NULL,
-    last_accessed_at INTEGER DEFAULT 0,
-    ip_banned INTEGER DEFAULT 0
+    id INTEGER PRIMARY KEY AUTOINCREMENT, -- ID incremental para cada plataforma
+    name TEXT NOT NULL, -- Nome de Marca da Plataforma
+    description TEXT, -- Descricao breve do conteúdo encontrado na plataforma e sua disponibilizacao
+    landing_page TEXT NOT NULL, -- URL da pagina inicial da plataforma
+    content_delivery_type TEXT NOT NULL, -- Tipo de entrega de conteúdo da plataforma API/HTML
+    last_accessed_at INTEGER DEFAULT 0, -- Qyabdi foi a ultima vez que a plataforma foi acessada
+    ip_banned INTEGER DEFAULT 0 -- Se o IP do cliente está banido da plataforma
 );
 
 CREATE TABLE IF NOT EXISTS Accounts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT,
-    is_valid INTEGER DEFAULT 0,
-    validated_at INTEGER DEFAULT 0,
-    has_authenticated INTEGER DEFAULT 0,
-    authenticated_at INTEGER DEFAULT 0,
-    authentication_token TEXT,
-    authentication_token_expires_at INTEGER DEFAULT 0,
-    platform_id INTEGER REFERENCES Platforms(id) ON DELETE CASCADE,
-    UNIQUE(username, platform_id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT, -- ID Incremental de cada conta adicionada ao sistema
+    username TEXT NOT NULL, -- Username ou Email da conta que possui o produto
+    password TEXT, -- Senha da conta que possui o produto
+    is_valid INTEGER DEFAULT 0, -- Se a conta está autenticando ou não
+    validated_at INTEGER DEFAULT 0, -- Quando a conta foi adicionada como válida
+    has_authenticated INTEGER DEFAULT 0, -- Se a conta possui sessão ativa na plataforma
+    authenticated_at INTEGER DEFAULT 0, -- Quando a sessão foi criada
+    authentication_token TEXT, -- O token de autorização da sessão
+    authentication_token_expires_at INTEGER DEFAULT 0, -- A data de expiração do Token
+    platform_id INTEGER REFERENCES Platforms(id) ON DELETE CASCADE, -- A id da plataforma que a conta pertence
+    UNIQUE(username, platform_id) -- Cada conta é única para cada plataforma.
 );
 
 
 CREATE TABLE IF NOT EXISTS Courses (
-    id INTEGER,
-    name TEXT NOT NULL,
-    description TEXT,
-    course_url TEXT NOT NULL,
-    downloaded_at INTEGER DEFAULT 0,
-    last_verified_at INTEGER DEFAULT 0,
-    platform_id INTEGER REFERENCES Platforms(id) ON DELETE CASCADE,
-    UNIQUE(id, platform_id)
+    id INTEGER, -- A ID do curso na plataforma
+    name TEXT NOT NULL, -- Nome do curso
+    description TEXT, -- Descrição do curso, se aplicável
+    course_url TEXT NOT NULL, -- A URL da landing page do curso
+    downloaded_at INTEGER DEFAULT 0, -- Se o download foi concluído, quando?
+    last_verified_at INTEGER DEFAULT 0, -- Data da última validação de conteúdo do curso
+    platform_id INTEGER REFERENCES Platforms(id) ON DELETE CASCADE, -- Plataforma a qual o curso pertence
+    UNIQUE(id, platform_id) -- Cada curso é único para cada plataforma.
 );

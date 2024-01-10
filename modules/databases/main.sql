2abb5ae2018eb6b552bffbfbf7ebf84cc8167cc2
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-01-10 14:00:35 -0300

    refactor: removido tipo MONITOR pois o programa funcionara em modo seguro

--- modules/databases/main.sql
+++ modules/databases/main.sql
@@ -6,11 +6,10 @@ CREATE TABLE IF NOT EXISTS Platforms (
     landing_page TEXT UNIQUE NOT NULL, -- URL da pagina inicial da plataforma
     content_delivery_type TEXT NOT NULL, -- Tipo de entrega de conteúdo da plataforma API/HTML
     has_drm INTEGER DEFAULT 0, -- Se a plataforma possui algum tipo de DRM
-    drm_type TEXT, -- Social (DRM FALSO), REAL (Widevine, PlayReady, FairPlay, etc), ou se é monitorado (MONITOR)
+    drm_type TEXT, -- Social (DRM FALSO), REAL (Widevine, PlayReady, FairPlay, etc)
     -- No caso de DRM FALSO, se a plataforma renderiza os dados do lado do cliente, ao baixar o arquivo é limpo, não teve
     -- nenhum procedimento para remoção. No caso de DRM REAL, será dado apenas um guia para o usuário seguir para conseguir
     -- realizar o download. Note que em momento algum o sistema irá remover quaisquer dados ocultos dos arquivos.
-    -- No caso de MONITOR, o sistema irá monitorar o acesso do usuário e verificar se o mesmo está baixando o conteúdo.
     last_accessed_at INTEGER DEFAULT 0, -- Qyabdi foi a ultima vez que a plataforma foi acessada
     ip_banned INTEGER DEFAULT 0 -- Se o IP do cliente está banido da plataforma
 );

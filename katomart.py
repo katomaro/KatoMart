7739b7bf9769f759cbbf7427d0d6badffe188fd0
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-01-12 13:14:58 -0300

    Agora começa minha própria tortura com web design

--- katomart.py
+++ katomart.py
@@ -1,14 +1,23 @@
 """Ponto de inicialização para um futuro server Flask"""
 
+from flask import Flask
+
 from modules.databases.manager_main import ManagerMain
 from modules.accounts.hotmart import Hotmart
 
+
+app = Flask(__name__)
+
+@app.route('/')
+def katomart_root():
+    return 'Bem vindo, gatinhos!'
+
+
 if __name__ == '__main__':
-    manager = ManagerMain()
+    # manager = ManagerMain()
     # print(manager.get_supported_platforms())
-    account = Hotmart('teste', 'teste', database_manager=manager)
-    account._login()
+    # account = Hotmart('teste', 'teste', database_manager=manager)
     # a = manager.insert_new_account(account)
     # print(a)
 
-    print("Goodbye World! Alprazolam 2mg é bom. Lembre-se que você deve ser acompanhado por um médico.")
+    app.run(debug=True, port=6102)

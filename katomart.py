883d08948a2b11cb4fab084c01e01f6a89c605ff
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-01-12 13:30:12 -0300

    Drac drac drac...VAMPIRE! Eh nois zeno https://github.com/dracula/dracula-ui

--- katomart.py
+++ katomart.py
@@ -1,16 +1,16 @@
 """Ponto de inicialização para um futuro server Flask"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 from modules.databases.manager_main import ManagerMain
 from modules.accounts.hotmart import Hotmart
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='modules/templates')
 
 @app.route('/')
 def katomart_root():
-    return 'Bem vindo, gatinhos!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':

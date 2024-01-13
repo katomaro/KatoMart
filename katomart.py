5cf3dba8d03bbd00baebc3776f6d6dcb903cece5
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-01-13 13:49:34 -0300

    Odeio web design

--- katomart.py
+++ katomart.py
@@ -10,10 +10,35 @@ app = Flask(__name__,
              template_folder='modules/front/templates',
                static_folder='modules/front/static'
             )
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 
 @app.route('/')
-def katomart_root():
-    return render_template('index.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/accounts')
+def accounts():
+    return render_template('accounts.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+@app.route('/log')
+def log():
+    return render_template('log.html')
+
+@app.route('/support')
+def support():
+    return render_template('support.html')
+
+
 
 
 if __name__ == '__main__':

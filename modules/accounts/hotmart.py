9a04a92c80039e2da19c0573961a56ad2b6cd537
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-01-14 19:19:27 -0300

    htm deprecated login info

--- modules/accounts/hotmart.py
+++ modules/accounts/hotmart.py
@@ -11,10 +11,35 @@ class Hotmart(Account):
                  database_manager=None):
         super().__init__(username=username, password=password, database_manager=database_manager)
         self.platform_id = 1
+
+        # TODO: Implementar login por SSO, essa URL é antiga e está deprecada
+        self.LOGIN_URL = 'https://sec-proxy-content-distribution.hotmart.com/club/security/oauth/token'
+        self.PRODUCTS_URL = ''
     
     def _login(self):
         """Realiza o login na conta."""
-        print(self.username, self.auth_token, self.password, self.refresh_token, self.platform_id)
+        login_data = self.session.post(self.LOGIN_URL, data={
+            'username': self.username,
+            'password': self.password,
+            'grant_type': 'password'
+            }
+        ).json()
+        if 'error' in login_data:
+            # TODO Passar o erro corretamente para o front-end posteriormente
+            raise Exception(login_data['error_description'])
+        
+        self.is_valid = True
+        self.validated_at = self.get_current_time()
+        self.has_authenticated = True
+        self.authenticated_at = self.get_current_time()
+        self.auth_token = login_data['access_token']
+        self.auth_token_expires_at = login_data['expires_in'] + self.get_current_time()
+        self.refresh_token = login_data['refresh_token']
+        self.other_data = {
+            'scope': login_data['scope'],
+            'token_type': login_data['token_type'],
+            'jti': login_data['jti']
+        }
     
     def _get_account_products(self):
         """Retorna os produtos da conta."""

8f0bbd4bc8224459f9bf4e620a52811d95fff57d
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-03-01 03:02:44 -0300

    refactor: base da htm

--- modules/accounts/hotmart.py
+++ modules/accounts/hotmart.py
@@ -2,47 +2,39 @@
 
 from .abstract import Account
 
-
 class Hotmart(Account):
-    """Esta é uma classe que representa um usuário da Hotmart."""
-    def __init__(self,
-                 username: str='',
-                 password: str='',
-                 database_manager=None):
-        super().__init__(username=username, password=password, database_manager=database_manager)
-        self.platform_id = 1
+    """
+    Representa um usuário da Hotmart, especializando a classe Account para
+    lidar com as especificidades desta plataforma.
+    """
+    
+    def __init__(self, username: str='', password: str='', database_manager=None):
+        """
+        Inicializa uma instância de Hotmart.
 
-        # TODO: Implementar login por SSO, essa URL é antiga e está deprecada
+        :param username: Nome de usuário ou e-mail.
+        :param password: Senha da conta.
+        :param database_manager: Gerenciador de banco de dados para esta conta.
+        """
+        super().__init__(username=username, password=password, database_manager=database_manager)
+        self.platform_id = 1  # Identificador único para a plataforma Hotmart.
         self.LOGIN_URL = 'https://sec-proxy-content-distribution.hotmart.com/club/security/oauth/token'
-        self.PRODUCTS_URL = ''
-    
-    def _login(self):
-        """Realiza o login na conta."""
-        login_data = self.session.post(self.LOGIN_URL, data={
-            'username': self.username,
-            'password': self.password,
-            'grant_type': 'password'
-            }
-        ).json()
-        if 'error' in login_data:
-            # TODO Passar o erro corretamente para o front-end posteriormente
-            raise Exception(login_data['error_description'])
         
-        self.is_valid = True
-        self.validated_at = self.get_current_time()
-        self.has_authenticated = True
-        self.authenticated_at = self.get_current_time()
-        self.auth_token = login_data['access_token']
-        self.auth_token_expires_at = login_data['expires_in'] + self.get_current_time()
-        self.refresh_token = login_data['refresh_token']
-        self.other_data = {
-            'scope': login_data['scope'],
-            'token_type': login_data['token_type'],
-            'jti': login_data['jti']
-        }
-    
+
+    def _login(self):
+        """
+        Realiza o login na conta da Hotmart, autenticando o usuário e obtendo tokens de acesso.
+        """
+        raise NotImplementedError("Método não implementado.")
+
     def _get_account_products(self):
-        """Retorna os produtos da conta."""
-    
-    def _get_product_information(self):
-        """Retorna as informações de um produto."""
+        """
+        Retorna os produtos associados à conta do usuário na Hotmart.
+        """
+        raise NotImplementedError("Método não implementado.")
+
+    def _get_product_information(self, product_id):
+        """
+        Retorna informações de um produto específico associado à conta do usuário.
+        """
+        raise NotImplementedError("Método não implementado.")

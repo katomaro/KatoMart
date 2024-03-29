c5db016335d55447c2bcc1978d310d8aa9cf9e8a
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-03-29 07:52:09 -0300

    fix: iter em bool lol

--- atualizar.py
+++ atualizar.py
@@ -26,7 +26,7 @@ def extract_zip(filename):
 
 def move_contents_from_folder(source_folder):
     for item in os.listdir(source_folder):
-        if item in should_keep(item):
+        if should_keep(item):
             continue
         full_item_path = os.path.join(source_folder, item)
         target_path = os.path.join(".", item)

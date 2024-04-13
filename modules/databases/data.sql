e0825d546b4bbc8ee5add3b55772e168fc81621d
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2024-04-13 02:09:22 -0300

    feat: add bento4 path to options for media decryption

--- modules/databases/data.sql
+++ modules/databases/data.sql
@@ -8,6 +8,7 @@ INSERT OR IGNORE INTO Settings (key, value) VALUES
 ('default_user_agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0'),
 ('download_widevine', '0'),
 ('widevine_cdm_path', ''),
+('bento4_toolbox_path', ''),
 ('scan_html_for_videos', '0'),
 ('use_custom_ffmpeg', '0'),
 ('custom_ffmpeg_path', 'SYSTEM'),
